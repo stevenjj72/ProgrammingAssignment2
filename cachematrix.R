86b0c0660892ae5c97cdf764211e4920b42f0a01
Stevenjj72 <steven.j.jones72@gmail.com>
2018-06-02 17:41:55 -0500

    This is only a test

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This assignment was accomplished by Steve Jones
+## 
+## This is version one of the function, made only to test git commands
+## If this is the final version I submit on before it is due, I've had a really bad weekend
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## Yes, I really intend to put code here
 }
 
 
